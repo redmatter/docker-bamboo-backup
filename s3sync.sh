c7e5ea2b7b6cb0588ed9f97a84dd2b0062c33ea5
Dino Korah <dino.korah@redmatter.com>
2016-04-25 18:15:55 +0100

    PLAT-7258 Typo fix

--- s3sync.sh
+++ s3sync.sh
@@ -4,13 +4,13 @@
 : ${AWS_SECRET_KEY:?"AWS Secret key not specified (AWS_SECRET_KEY)"}
 : ${GPG_PASSPHRASE:?"GPG Encryption passphrase not specified (GPG_PASSPHRASE)"}
 : ${AWS_S3_BUCKET:?"AWS Bucket name not specified (AWS_S3_BUCKET)"}
+: ${AWS_S3_BUCKET_PATH:=}
 
 sed -i "s~{{AWS_ACCESS_KEY}}~${AWS_ACCESS_KEY}~g;
     s~{{AWS_SECRET_KEY}}~${AWS_SECRET_KEY}~g;
     s~{{GPG_PASSPHRASE}}~${GPG_PASSPHRASE}~g;" \
         ${BAMBOO_BACKUP_HOME}/s3cfg.ini
 
-${AWS_S3_BUCKET_PATH:=}
 if [ -n "${AWS_S3_BUCKET_PATH}" ]; then
     AWS_S3_BUCKET_PATH=$(readlink -m "/${AWS_S3_BUCKET_PATH}/")
 fi
